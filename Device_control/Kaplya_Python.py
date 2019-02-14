b6a8559bab05edc134c1ab3cacf81155e39621c8
nata <nata.br@gmail.com>
2019-02-14 11:48:56 +0100

    precalculations for standartisation of peripherals

--- Device_control/Kaplya_Python.py
+++ Device_control/Kaplya_Python.py
@@ -7,9 +7,15 @@ from time import sleep
 import datetime 
 
 
-# constants
-PERIOD = 5
-MCP_pins_count = 8
+# constant
+PERIOD_1 = 10
+PERIOD_2 = 20
+MCP_pins_count = 4
+PIN_PUMP = 24
+PIN_SOL0 = 17
+PIN_SOL1 = 27
+PIN_SOL2 = 22
+PIN_SOL3 = 14
 
 MODE = "Relais test"    # comment   it to test sensor's data reception
                         # uncomment it to manipulate by relais
@@ -19,27 +25,41 @@ def firstTests():
     
     if 'MODE' in globals():    # manipulate by relais  
         
-        list_pins = [14, 15, 17, 18, 22, 23, 24, 27]
+        list_pins = [PIN_SOL0, PIN_SOL2, PIN_SOL2, PIN_SOL3, PIN_PUMP]
         sol_list = []
 
         for i in list_pins:
             sol_list.append(LED(i))
         
+        #while True:
+            #for el in sol_list:        
+            #    el.off()
+            #time.sleep(PERIOD)
+            #for el in sol_list:        
+            #    el.on()
+            #time.sleep(PERIOD)
         while True:
             for el in sol_list:        
+                el.on()
+
+            for el in sol_list:        
                 el.off()
-            time.sleep(PERIOD)
+            time.sleep(PERIOD_1)
+
             for el in sol_list:        
                 el.on()
-            time.sleep(PERIOD)
+            time.sleep(PERIOD_1)
+
     
-    else:        # take data from MCP3008   
+    else:        # take data from MCP3008
+        print(" 1   2   3   4 ")
+        print("================================")
         while True:     
             # cycle: check the data from each of MCP's pins 
             for i in range(0, MCP_pins_count):        
-                pot = MCP3008(i)
-                print("pin " + str(i) + " row value = " + str(pot.raw_value))                
-            print("================================")
+                pot = MCP3008(i)                
+                print(str(pot.raw_value), end = " ")            
+            print("")
             time.sleep(PERIOD)
 
 
