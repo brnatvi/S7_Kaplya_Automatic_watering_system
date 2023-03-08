33947689873c5f2b4510434f38e8e104ba2fd25d
nata <nata.br@gmail.com>
2023-03-08 14:25:28 +0100

    tests results + statistic

--- Device_control/Kaplya_Python.py
+++ Device_control/Kaplya_Python.py
@@ -4,10 +4,12 @@ from gpiozero import LED, MCP3008
 import sys
 import time
 from time import sleep
-import datetime 
+from datetime import datetime
+import csv
+import os
 
 
-# constant
+# constants
 PERIOD_1 = 10
 PERIOD_2 = 20
 MCP_pins_count = 4
@@ -20,7 +22,7 @@ PIN_SOL3 = 14
 MODE = "Relais test"    # comment   it to test sensor's data reception
                         # uncomment it to manipulate by relais
 
-
+# Tests sensors or solenoids + pump
 def firstTests():
     
     if 'MODE' in globals():    # manipulate by relais  
@@ -31,13 +33,6 @@ def firstTests():
         for i in list_pins:
             sol_list.append(LED(i))
         
-        #while True:
-            #for el in sol_list:        
-            #    el.off()
-            #time.sleep(PERIOD)
-            #for el in sol_list:        
-            #    el.on()
-            #time.sleep(PERIOD)
         while True:
             for el in sol_list:        
                 el.on()
@@ -60,9 +55,45 @@ def firstTests():
                 pot = MCP3008(i)                
                 print(str(pot.raw_value), end = " ")            
             print("")
-            time.sleep(PERIOD)
+            time.sleep(PERIOD_1)
 
+
+
+# Writes raw data from sensors to the .csv file 10 times with a pause of PERIOD_1 seconds
+def testSensors():
+    path_dir = os.path.abspath(os.path.join('..'))
+    path_dir = os.path.join(path_dir, 'Tests')
+    if (not os.path.exists(path_dir)):
+        os.mkdir(path_dir)
+    path = os.path.join(path_dir, 'sensors_test_moist_nonisolated.csv') 
+ 
+    with open(path, mode='a') as csvfile:
+            
+        writer = csv.writer(csvfile, delimiter = ",", lineterminator="\r")
+            
+        current_time = datetime.now()
+        time_stamp = current_time.timestamp()            
+        date_time = datetime.fromtimestamp(time_stamp)
+        str_date_time = date_time.strftime("%d-%m-%Y, %H:%M:%S")
+
+        writer.writerow([str_date_time])
+        buffer = []
+        count = 10
+        while (count > 0):
+            for i in range(0, MCP_pins_count):        
+                pot = MCP3008(i)                    
+                buffer.append(pot.raw_value)
+                    
+            print(buffer)                  
+            writer.writerow(buffer)
+            buffer.clear()
+            count -= 1
+            time.sleep(PERIOD_1)
+            
+        writer.writerow(buffer)
+        csvfile.close()
+    
 
 
 if __name__ == '__main__':
-    firstTests()
+    testSensors()
